602d2ea53396b43ff914f4c3fbfa0f9dc2167559
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-06-30 23:51:18 +0100

    implemented farenhiet converter in python

---
+++ src/celsuisToFarenheitConverter.py
@@ -0,0 +1,6 @@
+temperatureInCelsius: float = input("Enter temperature in celsius: ")
+temperatureInCelsius = float(temperatureInCelsius)
+
+temperatureInFarenheit = ((temperatureInCelsius * 9) / 5) + 32
+
+print("The temperature in farenheit is ", temperatureInFarenheit)
