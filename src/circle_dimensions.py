2b5031c2858b68de3bc085f5fec936cc45eb6c5e
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-07-01 12:09:56 +0100

    added files

---
+++ src/circle_dimensions.py
@@ -0,0 +1,11 @@
+import math
+
+radius: int = 2
+diameter: float = 2 * radius
+circumference: float = 2 * math.pi * radius
+area: float = math.pi * radius ** 2
+
+print(f"The diameter of the circle is {diameter}")
+print(f"The circumference of the circle is {circumference}")
+print(f"The area of the circle is {area}")
+
